a172cfb637bc169e54b36aae94679b84b5e1ce77
Paweł Madej <pawel.madej@amukana.pl>
2020-08-20 17:56:52 +0200

    Initial Commit

---
+++ Shared/ios14testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios14testApp.swift
+//  Shared
+//
+//  Created by Pawel Madej on 20/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct ios14testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
